331b3bc16a0a0e7e8b2e499466171c40e3f1b9e8
yuyingjiancn <yuyingjian@gmail.com>
2015-03-16 20:18:13 +0800

    0.0.1

---
+++ YuYingjian.Web.Flash/YuFlash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace YuYingjian.Web.Flash
+{
+    public static class YuFlash
+    {
+        /// <summary>
+        /// 使用方法：
+        /// 在逻辑代码中
+        /// YuFlash.Flash("success", "操作成功");
+        /// 在页面代码中
+        /// @if(YuFlash.HasFlash("success")){
+        ///     <p class="success">@YuFlash.Flash("success")</p>
+        /// }
+        /// </summary>
+        public static void Flash(string key, string message)
+        {
+            HttpCookie cookie = new HttpCookie(string.Format("yyj_flash_{0}", key));
+            cookie.Value = HttpUtility.UrlEncode(message, Encoding.UTF8);
+            cookie.Expires = DateTime.Now.AddMinutes(6);
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+
+        public static string Flash(string key)
+        {
+            var cookie = HttpContext.Current.Request.Cookies[string.Format("yyj_flash_{0}", key)];
+            var message = HttpUtility.UrlDecode(cookie.Value, Encoding.UTF8);
+            cookie.Expires = DateTime.Now.AddHours(-6);
+            HttpContext.Current.Response.Cookies.Add(cookie);
+            return message;
+        }
+
+        public static bool HasFlash(string key)
+        {
+            return HttpContext.Current.Request.Cookies.AllKeys.Contains(string.Format("yyj_flash_{0}", key));
+        }
+    }
+}
