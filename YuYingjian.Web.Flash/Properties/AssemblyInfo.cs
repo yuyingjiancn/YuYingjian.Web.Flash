5831a1dcf2064489b95a7aaf08b06530cf2a5874
yuyingjiancn <yuyingjian@gmail.com>
2015-03-16 20:24:45 +0800

    0.0.1

--- YuYingjian.Web.Flash/Properties/AssemblyInfo.cs
+++ YuYingjian.Web.Flash/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YuYingjian.Web.Flash")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("asp.net flash message")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yu YingJian")]
 [assembly: AssemblyProduct("YuYingjian.Web.Flash")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
